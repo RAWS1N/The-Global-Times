5402044ee49070155131b16dadfc1677714231b6
RAWSIN <diveshkumarsingariya@gmail.com>
2023-01-01 09:50:53 +0530

    network error fixed

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosServer = axios.create({
-  baseURL: "https://api.mediastack.com/v1",
+  baseURL: "http://api.mediastack.com/v1",
   params: {
     access_key: "eb722bb237f3e8fa71bc1fb7fe7a0810",
     sort: "published_desc",
